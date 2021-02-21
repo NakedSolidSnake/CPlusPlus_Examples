3f1139377dfa620f7e130c994d96c59f9bf76ae1
Cristiano Silva <cristianosstec@gmail.com>
2021-02-21 11:05:28 -0300

    improve guess game

--- 01-GuessGame/guess_game.cpp
+++ 01-GuessGame/guess_game.cpp
@@ -1,5 +1,50 @@
 #include <iostream>
+#include <limits>
 #include <ctime>
+
+class Game
+{
+    public:
+
+        enum difficulties{ easy , medium, hard};
+
+        Game(int max_tries) :        
+        max_tries(max_tries),
+        max_points(1000){
+
+        }
+
+        static Game getDifficulty(enum difficulties difficult){
+            switch (difficult)
+            {
+            case Game::difficulties::easy:
+                return Game(7);
+
+            case Game::difficulties::medium:
+                return Game(5);
+
+            case Game::difficulties::hard:
+                return Game(3);
+            
+            default:
+                return Game(5);
+            }
+        }
+
+        int getMaxTries() const
+        {
+            return this->max_tries;
+        }
+
+        int getMaxPoints() const
+        {
+            return this->max_points;
+        }
+
+    private:        
+        const int max_tries;
+        const int max_points;
+};
 
 void printTitleStyle(std::string message, const char decorate)
 {
@@ -23,9 +68,16 @@ int produceSecretNumber(int range)
 
 int getValue()
 {
-    int guess;
+    int guess = 0;
     std::cout << "Type your guess number: ";
-    std::cin >> guess;
+
+    while (!(std::cin >> guess))
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input.  Try again: ";
+    }
+
     return guess;
 }
 
@@ -50,16 +102,60 @@ bool evaluateGuess(int guess_number, int secret_number)
     return ret;
 }
 
-// int getDifficulty()
+char selectDifficulty()
+{
+    char choose;
+    bool keep = true;
+    std::cout << "Select the level : " << std::endl;
+    std::cout << "[q] - quit" << std::endl;
+    std::cout << "[e] - easy" << std::endl;
+    std::cout << "[m] - medium" << std::endl;
+    std::cout << "[h] - hard" << std::endl;
 
+    while(keep)
+    {
+        std::cin >> choose;
+        if(choose == 'q' || choose == 'e' || choose == 'm' || choose == 'h')
+        {
+            keep = false;
+        }
+        else {
+            std::cout << "This option doesn't exist. Please select a valid option" << std::endl;
+        }
+    }    
+
+    return choose;
+}
+
+Game::difficulties convertSelection(char choose)
+{
+    Game::difficulties difficulty = Game::difficulties::medium;
+    if(choose == 'q')
+    {
+        std::cout << "Quitting the game." << std::endl;
+        exit(0);
+    }
+    else if(choose == 'e')
+        difficulty = Game::difficulties::easy;
+
+    else if(choose == 'm')
+        difficulty = Game::difficulties::medium;
+
+    else if(choose == 'h')
+        difficulty = Game::difficulties::hard;
+
+    return difficulty;
+}
 
 int main(int argc, char const *argv[])
 {
     printTitleStyle("Guess the Secret Number", '@');
-    int tries = 3;
-    const int max_tries = tries;
-    double max_points = 1000.0;
-    double points = max_points;
+
+    int difficulty = selectDifficulty();
+    Game game = Game::getDifficulty(convertSelection(difficulty));
+
+    int tries = game.getMaxTries();   
+    double points = game.getMaxPoints();
 
     srand(time(nullptr));
 
@@ -72,7 +168,7 @@ int main(int argc, char const *argv[])
             break;
         }
         tries--;
-        points -= (max_points / max_tries);
+        points -= (game.getMaxPoints() / game.getMaxTries());
     }
 
     if(tries == 0)
